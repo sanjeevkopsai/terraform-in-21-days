a51208a6d9f802c00b2e6208d256ed32de47ddb9
Sanjeev M <132325692+sanjeevkopsai@users.noreply.github.com>
2023-05-16 07:22:33 -0700

    Add SSM changes (#15)

--- level2/instance.tf
+++ level2/instance.tf
@@ -18,91 +18,10 @@ data "aws_ami" "amazonlinux" {
 
 }
 
-# resource "aws_instance" "public" {
-#   count = 2
-
-#   ami                         = data.aws_ami.amazonlinux.id
-#   associate_public_ip_address = true
-#   instance_type               = "t3.micro"
-#   key_name                    = "sanjeevk-tf"
-#   vpc_security_group_ids      = [aws_security_group.public.id]
-#   subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[count.index]
-#   user_data                   = file("user-data.sh")
-
-#   tags = {
-#     Name = "${var.env_code}-public"
-#   }
-# }
-
-# resource "aws_security_group" "public" {
-#   name        = "${var.env_code}-public"
-#   description = "Allow inbound traffic"
-#   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-#   ingress {
-#     description = "SSH from public"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["71.198.160.159/32"]
-#   }
-
-#   ingress {
-#     description = "HTTP from public"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     security_groups = [aws_security_group.load_balancer.id]
-#   }
-
-#   ingress {
-#     description = "HTTP from load balancer"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "${var.env_code}-public"
-#   }
-# }
-
-# resource "aws_instance" "private" {
-#   count = 2
-
-#   ami                    = data.aws_ami.amazonlinux.id
-#   instance_type          = "t3.micro"
-#   key_name               = "sanjeevk-tf"
-#   vpc_security_group_ids = [aws_security_group.private.id]
-#   subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[count.index]
-#   user_data              = file("user-data.sh")
-
-#   tags = {
-#     Name = "${var.env_code}-private"
-#   }
-# }
-
 resource "aws_security_group" "private" {
   name        = "${var.env_code}-private"
   description = "Allow VPC traffic"
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
-  }
 
   ingress {
     description     = "HTTP from load balancer"
@@ -125,23 +44,23 @@ resource "aws_security_group" "private" {
 }
 
 resource "aws_launch_configuration" "main" {
-  name_prefix = "${var.env_code}-"
-  image_id  = data.aws_ami.amazonlinux.id
-  instance_type = "t3.micro"
-  security_groups = [aws_security_group.private.id]
-  user_data = file("user-data.sh")
-  key_name = "sanjeevk-tf"
+  name_prefix          = "${var.env_code}-"
+  image_id             = data.aws_ami.amazonlinux.id
+  instance_type        = "t3.micro"
+  security_groups      = [aws_security_group.private.id]
+  user_data            = file("user-data.sh")
+  iam_instance_profile = aws_iam_instance_profile.main.name
 }
 
 resource "aws_autoscaling_group" "main" {
-  name = var.env_code
-  min_size = 2
+  name             = var.env_code
+  min_size         = 2
   desired_capacity = 2
-  max_size = 4
+  max_size         = 4
 
-  target_group_arns = [aws_lb_target_group.main.arn]
+  target_group_arns    = [aws_lb_target_group.main.arn]
   launch_configuration = aws_launch_configuration.main.name
-  vpc_zone_identifier = data.terraform_remote_state.level1.outputs.private_subnet_id
+  vpc_zone_identifier  = data.terraform_remote_state.level1.outputs.private_subnet_id
 
   tag {
     key                 = "Name"
